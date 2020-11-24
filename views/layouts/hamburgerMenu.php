d1bd1961f1e92840152e22fe7ec87bf3710d843a
Yaroslav Livchak <sljar147@gmail.com>
2020-11-24 12:47:13 +0200

    'contakt'

--- views/layouts/hamburgerMenu.php
+++ views/layouts/hamburgerMenu.php
@@ -1,18 +1,17 @@
 <div class="col-lg-0 col-md-0 col-sm-2 col-xs-2">
 	<?php
-		$content ="<div class='popover-text btn-group btn-group-justified' role='group' aria-label='...'>
-		    <a href='/' title='ГОЛОВНА' class='btn btn-info'>ГОЛОВНА</a>
-		    <a href='/relax/1' title='ДОЗВІЛЛЯ' class='btn btn-info'>ДОЗВІЛЛЯ</a>
+		$content ="<div class='text-center' >
+		    <a href='/' title='ГОЛОВНА' class='text-menu btn btn-info'>ГОЛОВНА</a>
+		    <a href='/relax/1' title='ДОЗВІЛЛЯ' class='text-menu btn btn-info'>ДОЗВІЛЛЯ</a>
+		    <a href='/contakt' title='контакти' class='text-menu btn btn-info'>контакти</a>
 		</div>";
 		$nm = "меню";
 	?>
-	<div class='showSmall'>		
-		<div class='text-left'>
+	<div class='showSmall text-left'>		
 			<a style="margin-top:0px;" href="#" tabindex="0" data-trigger="focus" class="btn btn-lg"
 				data-container="body" role="button" data-toggle="popover" 
 				data-placement="bottom" data-html="true" title = "<?=$nm?>" data-content="<?=$content?>">
 				<i class="fa fa-bars fa-1x"></i>
-			</a>
-		</div>		
+			</a>			
 	</div>
 </div>
