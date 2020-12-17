eb657c3c184cb8b1a66bcfcbbc1f0440ace15864
Yaroslav Livchak <sljar147@gmail.com>
2020-12-17 14:18:50 +0200

    'fullNew'

--- views/news/fullNew.php
+++ views/news/fullNew.php
@@ -5,10 +5,15 @@
 		po.src = 'https://apis.google.com/js/platform.js';
 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 	})();
-</script> -->
-
-<?=Auxiliary::showReklRand()?>
-
+</script> -->			
+<div class="container-fluid">
+	<div class="row">
+		<?php include 'views/layouts/hamburgerMenu.php';?>						
+		<div class="col-lg-12 col-md-12 col-sm-9 col-xs-9">	
+			<?=Auxiliary::showReklRand()?>						
+		</div>				
+	</div>
+</div>
 <?php include_once 'views/news/newsMsg.php';?>
 <?php include_once 'views/news/socialMedia.php';?>
 <?php include_once 'views/news/newsElse.php';?>
