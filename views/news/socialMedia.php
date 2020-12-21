35a4b16da0334f008d6c6218626986bb46af67d8
Yaroslav Livchak <sljar147@gmail.com>
2020-12-21 17:41:16 +0200

    'media'

--- views/news/socialMedia.php
+++ views/news/socialMedia.php
@@ -10,20 +10,22 @@
 
 <div class="container-fluid">
 	<div class="row-fluid">
-		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-			<span>
-				<a href="https://twitter.com/share" class="twitter-share-button" data-via="sljar147">Tweet</a>
+		<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+			
+				<a href="https://twitter.com/share" class="twitter-share-button" data-via="sljar147" target="_blank">
+					<img src="/image/Iconfinder_Twitter.png" width="50" height="50" alt="Tweeter">
+				Tweet</a>
 				<script type="text/javascript">tweet()</script>
-			</span>
+			
 		</div>
-		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+		<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
 			<span>
 				<div class="fb-like" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 			</span>
 		</div>
-		<!-- Розмістіть цей тег у місці, де потрібно відтворити Кнопка +1 -->
+		<!-- Розмістіть цей тег у місці, де потрібно відтворити Кнопка +1 
 		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
 			<div class="g-plusone" data-annotation="inline" data-width="300"></div>
-		</div>    
+		</div>  -->  
 	</div>
 </div>
