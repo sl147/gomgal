b73fa687c90807635e42b6538fca24266ed52594
Yaroslav <sl147@ukr.net>
2020-12-29 17:03:23 +0200

    'fb2'

--- views/news/socialMedia.php
+++ views/news/socialMedia.php
@@ -1,5 +1,4 @@
 <div id="fb-root"></div>
-<!-- <script async defer crossorigin="anonymous" src="https://connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v9.0&appId=554682698014491&autoLogAppEvents=1" nonce="M3IBbmkA"></script> -->
 
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v9.0&appId=553789431437151&autoLogAppEvents=1" nonce="CvszNQhn"></script>
 
@@ -16,12 +15,9 @@
 		
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-			<!-- <div class="fb-like" data-href="<?php echo $fb?>" data-width="450" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div> -->
+			<div class="fb-like" data-href="<?php echo $fb?>" data-width="500" data-layout="standard" data-action="like" data-size="small" data-share="true" data-lazy="true"></div>
+			<div class="fb-comments" data-href="<?php echo $fb?>" data-width="550" data-numposts="5"></div>
 
-			<div class="fb-like" data-href="<?php echo $fb?>" data-width="500" data-layout="standard" data-action="like" data-size="small" data-share="true"></div>
-<!-- 			<span>
-				<div class="fb-like" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
-			</span> -->
 		</div>
 		<!-- Розмістіть цей тег у місці, де потрібно відтворити Кнопка +1 
 		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
