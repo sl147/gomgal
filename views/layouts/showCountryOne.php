a8fe78a7674963032121a973d247c3de67b1115a
Yaroslav <sl147@ukr.net>
2021-01-13 11:25:42 +0200

    'fb'

--- views/layouts/showCountryOne.php
+++ views/layouts/showCountryOne.php
@@ -1,4 +1,5 @@
 <div class="text-center">
+	<?=Auxiliary::showCountry('https://covid19.rnbo.gov.ua/','COVID-19','COVID-19')?>
 	<?=Auxiliary::showCountry('http://www.president.gov.ua/','Президент','Президент')?>
 	<?=Auxiliary::showCountry('http://rada.gov.ua/','Верховна Рада','Верховна Рада')?>
 	<?=Auxiliary::showCountry('http://www.kmu.gov.ua/','Кабінет міністрів','Кабінет міністрів')?>
--- views/user/index.php
+++ views/user/index.php
@@ -37,3 +37,52 @@
     забули логін(пароль)
   </a>
 </form>
+
+<!-- <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '553789431437151',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<script>    
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+    //alert(response.status)
+});
+</script>
+
+<script>
+<fb:login-button 
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
+</script>
+
+<script>
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+</script>
+
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v9.0&appId=553789431437151&autoLogAppEvents=1" nonce="tWtmHtQ1"></script>
+
+<div class="fb-login-button" data-width="400" data-size="large" data-button-type="continue_with" data-layout="rounded" data-auto-logout-link="true" data-use-continue-as="true"></div> -->
