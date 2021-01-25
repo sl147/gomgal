3902a71d2fd1cd63aac865709e16bcfe6717e26c
Yaroslav <sl147@ukr.net>
2021-01-25 14:38:00 +0200

    'fullnews'

--- views/news/newsEditOne.php
+++ views/news/newsEditOne.php
@@ -1,5 +1,6 @@
 <?php include 'views/layouts/headerAdmin.php';?>
-<!-- <script src="/ckeditor/ckeditor.js"></script> -->
+<script src="/ckeditor/ckeditor.js"></script>
+<!-- <script src="//cdn.ckeditor.com/4.15.1/standard/ckeditor.js"></script> -->
 <!-- <script src="/ckeditor5-build-classic/ckeditor.js"></script> -->
 <!-- <script src="https://cdn.ckeditor.com/ckeditor5/23.1.0/classic/ckeditor.js"></script> -->
 <h2 class="text-center"><?= $title?></h2>
@@ -65,9 +66,9 @@
 	</div>			
 </div>		
 <?php include 'views/layouts/footerAdmin.php';?>
-<!-- <script type='text/javascript'>
+<script type='text/javascript'>
 	CKEDITOR.replace('msg');
-</script> -->
+</script>
 <!-- <script>
         ClassicEditor
             .create( document.querySelector( '#editor' ) )
