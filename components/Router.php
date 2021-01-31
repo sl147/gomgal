28e1cbf99f20994739f2d84d47dc6e8bf74f9d7b
Yaroslav <sl147@ukr.net>
2021-01-31 13:51:43 +0200

    'route'

--- components/Router.php
+++ components/Router.php
@@ -14,32 +14,44 @@ class Router {
 		}
 	}
 
-	public function run() {
+	public function run()
+	{
 		$uri = $this->getURI();
-		foreach ($this->routes as $uriPattern => $path) {
-			if (preg_match("`$uriPattern`", $uri)) {
+		foreach ($this->routes as $uriPattern => $path)
+		{
+			if (preg_match("`$uriPattern`", $uri))
+			{
 				$internalRoute = preg_replace("`$uriPattern`", $path, $uri);
-				//echo "<br>internalRoute - $internalRoute";
+				//echo "<br>internalRoute - $internalRoute<br>";
 				$segments = explode("/", $internalRoute);
-				//ім'я сконтролера
 				$controllerName = array_shift($segments)."Controller";
 				$controllerName = ucfirst($controllerName);
-				//echo "<br>cont - $controllerName";
-				// action
+				//echo "<br>controllerName - $controllerName";
 				$actionName = "action".ucfirst(array_shift($segments));
 				//echo "<br>actionName - $actionName";
-				// параметри
 				$parametrs = $segments;
 				$ControllerFile = "controllers/".$controllerName.".php";
 				//echo "<br>File - $ControllerFile";
-				if (file_exists($ControllerFile)) {
+				if (file_exists($ControllerFile))
+				{
 					include_once ($ControllerFile);
 					$controllerObject = new $controllerName;
-					$result = call_user_func_array(array($controllerObject,$actionName), $parametrs);
-					if ($result != NULL) break;
+					if (method_exists($controllerObject,$actionName))
+					{
+						$result = call_user_func_array(array($controllerObject,$actionName), $parametrs);	
+						if ($result != NULL) break;
+					}
+					else
+					{
+						$ControllerFile = 'controllers/SiteController.php';
+						$controllerObject = new $controllerName;
+						$actionName = 'actionIndex';
+						$result = call_user_func_array(array($controllerObject,$actionName), $parametrs);
+						if ($result != NULL) break;	
+					}
 				}
 			}
-			
+				
 		}
 	}
 }
