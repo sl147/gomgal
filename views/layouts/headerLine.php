e89f68327f5cb6a781aa36fd3224b5a29c595b4d
Yaroslav <sl147@ukr.net>
2021-05-04 18:08:25 +0300

    'fb'

--- views/layouts/headerLine.php
+++ views/layouts/headerLine.php
@@ -8,13 +8,13 @@
 					<div style="padding-left: 60px; padding-top:5px; color:grey">
 						<script type="text/javascript">datecurr()</script>	
 					</div>
-					
+
 				</div>
 
 				<div class="col-lg-2 col-md-2 col-sm-6 col-xs-12"></div>
 					<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
 						<a href="/insurance" target="_blank">
-							<img src="../image/autosmall.png" height="80" width="auto" alt="">
+							<img src="/image/autosmall.png" height="80" width="auto" alt="">
 						<a/>
 				</div>
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
@@ -35,6 +35,11 @@
 						<?else :?>
 							Привіт <b>відвідувач</b><br>	
 						<?php endif; ?>
+					</div><br><br>
+					<div class="text-right">
+						<a href="https://www.facebook.com/Гомін-Галичини-398431253663801" target="_blank">
+							<img src="/image/fb.png" height="30" width="auto" alt="">
+						</a>
 					</div>							
 				</div>
 			</div>
