71604b5254ba602110bb818c7e9daa603678ce5e
sl147 <sl147u@ukr.net>
2023-12-13 10:45:33 +0100

    1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,61 +1,30 @@
 <?php
 
-
-
-class SiteController
-
-{	
+class SiteController {	
 
 	use traitAuxiliary;
 
-
-
 	public function actionIndex($page = 1)	{
-
 		$mt      = new MetaTags();
-
 		$news    = new News();
-
 		$page    = $this->getIntval($page);	
-
 		$month   = date('n');
-
 		$year    = date('Y');
-
 		$topNews = $news->getNewsTop();
-
 		$total   = $news->getTotalNews($month, $year);
-
 		$allNews = $news->getLatestNews($month, $year, $page);
-
 		if (count($allNews) == 0) {
-
 			$month--;
-
 			$month = ($month == 0) ? 12 : $month;
-
 			$allNews = $news->getLatestNews($month, $year, $page);			 
-
 		}
-
 		unset($news);
-
 		$pagination = new Pagination($total, $page, SHOWNEWS_BY_DEFAULT, 'page-');
-
 		$siteFile   = 'views/site/index.php';
-
 		$siteSmall  = 'views/layouts/leftSide.php';
-
 		$meta       = $mt->getMTagsByUrl(trim($_SERVER["REQUEST_URI"],'/'));
-
 		require_once ('views/layouts/siteMain.php');
-
 		unset($pagination);
-
 		return true;
-
 	}
-
-}	
-
-?>
+}
