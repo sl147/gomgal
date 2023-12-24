46af537dc0051a8036294f0766ad49465ed057d4
sl147 <sl147u@ukr.net>
2023-12-24 22:46:20 +0100

    1

--- models/Relax.php
+++ models/Relax.php
@@ -1,26 +1,21 @@
 <?php
 
-//declare(strict_types=1);
-
 /**
 * 
 */
 
-class Relax extends classGetDB
-{	
+class Relax extends classGetDB {	
 
 	use traitAuxiliary;
 
-	public function getRelaxId($id)
-	{
+	public function getRelaxId( int $id) :int {
 		$getData = new classGetData('catrelax');
 		$cat     = $getData->getDataFromTableByNameFetch ($this->getIntval($id),'idrl');
 		unset($getData);
-		return $cat;
+		return (int) $cat;
 	}
 
-	public function getRelax()
-	{
+	public function getRelax() :array {
 		$getData = new classGetData('catrelax');
 		$result  = $getData->getDataFromTable(2);
 		unset($getData);
@@ -30,11 +25,23 @@ class Relax extends classGetDB
 			$relaxList[$i]['name'] = $row['namerl'];
 			$i++;
 		}
-		return $relaxList ?? [];
+		return (array) $relaxList ?? [];
 	}
 
-	public function getAnList()
-	{
+	public function getThemeAn() :array {
+		$getData = new classGetData('catan');
+		$result  = $getData->getDataFromTable(2);
+		unset($getData);
+		$i       = 1;
+		while ($row = $result->fetch()) {			
+			$relaxList[$i]['id']   = $row['idrl'];
+			$relaxList[$i]['name'] = $row['namerl'];
+			$i++;
+		}
+		return (array) $relaxList ?? [];
+	}
+
+	public function getAnList() :array {
 		$result = $this->getDB("SELECT * FROM catan ORDER BY namerl");
 		$i      = 1;
 		while ($row = $result->fetch()) {			
@@ -42,32 +49,29 @@ class Relax extends classGetDB
 			$relaxList[$i]['name'] = $row['namerl'];
 			$i++;
 		}
-		return $relaxList ?? [];
+		return (array) $relaxList ?? [];
 	}
 
 
-	private function getRelaxMsg($cat)
-	{
+	private function getRelaxMsg( int $cat) :array {
 		$cat    = $this->getIntval($cat);
 		$result = $this->getDB("SELECT * FROM msgs_relax WHERE category=$cat and CHAR_LENGTH(msg)<400 and countrl>10");
 		while ($row = $result->fetch()) {			
 			$list[] = $row;
 		}
-		return $list ?? [];
+		return (array) $list ?? [];
 	}
 
-	public function getRelaxRandom($cat)
-	{
+	public function getRelaxRandom( int $cat) :string{
 		$arr = self::getRelaxMsg($cat);
 		if (count($arr) > 0) {
 			$j   = rand (0,count($arr)-1);
-			return $arr[$j]['msg'];
+			return (string) $arr[$j]['msg'];
 		}
-		return ""; 
+		return (string) ""; 
 	}
 
-	private function getCountRl() :int 
-	{
+	private function getCountRl() :int {
 		$result = $this->getDB("SELECT countrl FROM msgs_relax");
 		while ($row = $result->fetch()) {			
 			$list[] = $row['countrl'];
@@ -75,49 +79,44 @@ class Relax extends classGetDB
 		return (int) max($list);
 	}
 
-	public function addNewAn($teman, string $msg)
-	{
+	public function addNewAn( int $teman, string $msg) :bool{
 		$teman   = $this->getIntval($teman);
 		$countrl = $this->getCountRl() + 1;
         $sql     = "INSERT INTO msgs_relax (teman, msg, countrl) VALUES(:teman, :msg, :countrl )";
         $result  = $this->getPrepareSQL($sql);
-        $result -> bindParam(':teman',   $teman, PDO::PARAM_STR);
-        $result -> bindParam(':msg',     $msg,   PDO::PARAM_STR);
-        $result -> bindParam(':countrl', $countrl, PDO::PARAM_STR);
+        $result -> bindParam(':teman',   $teman,   PDO::PARAM_INT);
+        $result -> bindParam(':msg',     $msg,     PDO::PARAM_STR);
+        $result -> bindParam(':countrl', $countrl, PDO::PARAM_INT);
 
         return $result -> execute();		
 	}
 
-	public function getRelaxAll($page)
-	{
+	public function getRelaxAll( int $page) :array {
 		$getData = new classGetData('msgs_relax');
 		$offset  = ($this->getIntval($page) - 1) * SHOWRELAX_BY_DEFAULT;
 		$comList = $getData->getDataByOffset ('id',SHOWRELAX_BY_DEFAULT,$offset);
 		unset($getData);
-		return $comList;
+		return (array) $comList;
 	}
 
-	public function getRelaxOne($id)
-	{
+	public function getRelaxOne( int $id) :array {
 		$getData = new classGetData('msgs_relax');
 		$comList = $getData->getDataFromTableByNameFetch ($this->getIntval($id),'id');
 		unset($getData);
-		return $comList;
+		return (array) $comList;
 	}
 
-	public function updateRelax($id, $msg, $cat)
-	{
+	public function updateRelax( int $id, string $msg, int $cat) :bool {
 		$cat    = $this->getIntval($cat);
-		$result = $this->getPrepareSQL("UPDATE msgs_relax SET msg = :msg, category = :cat".$this->formSql("id",$this->getIntval($id)));
-		$result -> bindParam(':msg',   $msg,   PDO::PARAM_STR);
-		$result -> bindParam(':cat',   $cat,   PDO::PARAM_STR);
+		$result = $this->getPrepareSQL("UPDATE msgs_relax SET msg = :msg, category = :cat".$this->formSql( "id", $this->getIntval($id)));
+		$result -> bindParam(':msg',   $msg, PDO::PARAM_STR);
+		$result -> bindParam(':cat',   $cat, PDO::PARAM_INT);
 		return $result -> execute();			
 	}
 
-	public function updateCountRelax($id, $count)
-	{
-		$result = $this->getPrepareSQL("UPDATE msgs_relax SET countrl = :count".$this->formSql("id",$this->getIntval($id)));
-		$result -> bindParam(':count',   $count,   PDO::PARAM_STR);
+	public function updateCountRelax( int $id, int $count) :bool {
+		$result = $this->getPrepareSQL("UPDATE msgs_relax SET countrl = :count".$this->formSql( "id", $this->getIntval($id)));
+		$result -> bindParam(':count',   $count, PDO::PARAM_INT);
 		return $result -> execute();			
 	}
 }
