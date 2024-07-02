04d0ea7a8fa21850dd3bbc9aba03ae5a72c75f4f
sl147 <sl147u@ukr.net>
2024-07-02 13:54:01 +0200

    1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -10,19 +10,19 @@ class SiteController {
 		$page    = $this->getIntval($page);	
 		$month   = date('n');
 		$year    = date('Y');
-		$topNews = $news->getNewsTop();
-		$total   = $news->getTotalNews($month, $year);
+		$topNews = $news->getNewsTop();		
 		$allNews = $news->getLatestNews($month, $year, $page);
 		if (count($allNews) == 0) {
 			$month--;
 			$month = ($month == 0) ? 12 : $month;
 			$allNews = $news->getLatestNews($month, $year, $page);			 
 		}
+		$total   = $news->getTotalNews($month, $year);
 		unset($news);
 		$pagination = new Pagination($total, $page, SHOWNEWS_BY_DEFAULT, 'page-');
 		$siteFile   = 'views/site/index.php';
 		$siteSmall  = 'views/layouts/leftSide.php';
-		$meta       = $this->getMeta();//
+		$meta       = $this->getMeta();
 		require_once ('views/layouts/siteMain.php');
 		unset($pagination);
 		return true;
